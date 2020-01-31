0a7a9b5511afa17d1357e0e69e7e43c6419cf06c
gaurav2907 <gaurav.alwayshappening@hotmail.com>
2020-01-31 17:36:32 +0530

    zuul gateway

---
+++ src/test/java/com/example/demo/BootGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
